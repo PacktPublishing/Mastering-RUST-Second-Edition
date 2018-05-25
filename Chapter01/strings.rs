9b3f4893dcfd5c79ab67bc87e5a4dcfc6c7d02ce
Rahul Sharma <rsconceptx@gmail.com>
2018-05-25 21:46:10 +0530

    Add strings.rs

---
+++ Chapter01/strings.rs
@@ -0,0 +1,9 @@
+// strings.rs
+
+fn main() {
+    let question = "How are you ?";            // a &'static str type   
+    let person: String = "Bob".to_string();
+    let namaste = String::from("नमस्ते");        // unicodes yay! 
+
+    println!("{}! {} {}", namaste, question, person);
+}
