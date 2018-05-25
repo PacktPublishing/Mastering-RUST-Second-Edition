586f9d22befa18b75ea655c36751797f56bf1dc3
Rahul Sharma <rsconceptx@gmail.com>
2018-05-25 23:31:31 +0530

    Add structs.rs

---
+++ Chapter01/structs.rs
@@ -0,0 +1,25 @@
+
+struct Player { 
+    name: String,
+    iq: u8,
+    friends: u8,
+    score: u16
+}
+
+fn bump_player_score(mut player: Player, score: u16) {
+    player.score += 120;
+    println!("Updated player stats:");
+    println!("Name: {}", player.name);
+    println!("IQ: {}", player.iq);
+    println!("Friends: {}", player.friends);
+    println!("Score: {}", player.score);
+}
+
+fn main() { 
+   let player = Player { name: "Alice".to_string(),
+                          iq: 171,
+                          friends: 134, 
+                          score: 1129 };
+
+   bump_player_score(player, 120);
+}  
