fc4f1fb8fe60547d7b8f9d8b63ff7cae828aaa62
Rahul Sharma <rsconceptx@gmail.com>
2018-05-26 14:15:28 +0530

    Add hashmaps.rs

---
+++ Chapter01/hashmaps.rs
@@ -0,0 +1,18 @@
+
+use std::collections::HashMap; 
+
+fn main() { 
+    let mut fruits = HashMap::new(); 
+    fruits.insert("apple", 3);
+    fruits.insert("mango", 6);
+    fruits.insert("orange", 2);
+    fruits.insert("avocado", 7);
+    for (k, v) in &fruits {
+        println!("I got {} {}", v, k);
+    }
+
+    fruits.remove("orange");
+    let old_avocado = fruits["avocado"];
+    fruits.insert("avocado", old_avocado + 5);
+    println!("\nI now have {} avocados", fruits["avocado"]);
+}
