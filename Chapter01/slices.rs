69087769e8846bcfca481698a6a0a5571ad3c319
Rahul Sharma <rsconceptx@gmail.com>
2018-05-26 11:59:57 +0530

    Add slices.rs

---
+++ Chapter01/slices.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut numbers: [u8; 4] = [1,2,3,4];
+    {
+        let all: &[u8] = &numbers[..];
+        println!("All of them: {:?}", all);
+    }
+
+    {
+        let first_two: &mut [u8] = &mut numbers[0..2];
+        first_two[0] = 100;
+        first_two[1] = 99;
+    }
+
+    println!("Look ma! I can modify through slices: {:?}", numbers);
+}
