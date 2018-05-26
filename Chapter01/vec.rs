f4fcb5c3d36b01459956bff466256d676b9005fe
Rahul Sharma <rsconceptx@gmail.com>
2018-05-26 13:37:20 +0530

    Add vec.rs

---
+++ Chapter01/vec.rs
@@ -0,0 +1,17 @@
+
+fn main() { 
+  let mut numbers_vec: Vec<u8> = Vec::new(); 
+  numbers_vec.push(1); 
+  numbers_vec.push(2); 
+
+  let mut vec_with_macro = vec![1]; 
+  vec_with_macro.push(2);
+
+    let message = if numbers_vec == vec_with_macro {
+        "They are equal"
+    } else {
+        "Nah! They look different to me"
+    };
+
+  println!("{} {:?} {:?}", message, numbers_vec, vec_with_macro); 
+}
