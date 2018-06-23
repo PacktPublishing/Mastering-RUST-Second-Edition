e5352973c40fe2446815b2775d4fb9f516f0b433
Rahul Sharma <rsconceptx@gmail.com>
2018-06-23 23:07:09 +0530

    update Chapter01/match_expression.rs

--- Chapter01/match_expression.rs
+++ Chapter01/match_expression.rs
@@ -1,20 +1,18 @@
+// match_expression.rs
 
 fn main() {
     let request_code = 200;
     match request_code {
-
         200 => println!("Success"),
-
         404 => println!("Not Found"),
-        
         // can also use match guards
-        code if (code > 400 || code <= 500) => println!("Server Error {}", code),
-
-        error => {
+        code if (code > 400 || code <= 500) => {
             // multiline match arm
-            let error_str = format!("Network error! Reason: {}", error);
+            println!("Server Error {}", code);
+        }
+        error => {
+            let error_str = format!("Network error: {}", error);
             println!("{}", error_str);
         }
-
     }
 }
