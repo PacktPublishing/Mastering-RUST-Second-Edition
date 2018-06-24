25db74da9366a6c4a246ed960f4b0202feb04355
Rahul Sharma <rsconceptx@gmail.com>
2018-06-24 07:56:08 +0530

    update Chapter01/enum.rs

--- Chapter01/enums.rs
+++ Chapter01/enums.rs
@@ -1,39 +1,35 @@
+// enums.rs
 
-#[derive(Debug)]
-enum Direction {
-  N,
-  NE,
-  E,
-  SE,
-  S,
-  SW,
-  W,
-  NW,
+
+enum Direction { 
+    N, 
+    E, 
+    S, 
+    W
 }
 
 enum PlayerAction {
-  Move {
-    direction: Direction,
-    speed: u8,
-  },
-  Wait,
-  Attack(Direction),
+    Move {
+        direction: Direction,
+        speed: u8
+    },
+    Wait, 
+    Attack(Direction)   
 }
 
 fn main() {
-  let simulated_player_action = PlayerAction::Move {
-    direction: Direction::NE,
-    speed: 2,
-  };
-
-  match simulated_player_action {
-    PlayerAction::Wait => println!("Player wants to wait"),
-    PlayerAction::Move { direction, speed } => {
-      println!("Player wants to move in direction {:?} with speed {}",
+    let simulated_player_action = PlayerAction::Move {
+        direction: Direction::N,
+        speed: 2,
+    };
+    match simulated_player_action {
+        PlayerAction::Wait => println!("Player wants to wait"),
+        PlayerAction::Move { direction, speed } => {
+          println!("Player wants to move in direction {:?} with speed {}",
                 direction, speed)
-    }
-    PlayerAction::Attack(direction) => {
-      println!("Player wants to attack direction {:?}", direction)
-    }
-  };
+        }
+        PlayerAction::Attack(direction) => {
+            println!("Player wants to attack direction {:?}", direction)
+        }
+    };
 }
