ea61dabcff3e6c89a8cdbdd7b3ca359cfaf441ac
Rahul Sharma <rsconceptx@gmail.com>
2018-07-08 23:26:34 +0530

    Add finished photocat project

--- Chapter02/photocat/src/main.rs
+++ Chapter02/photocat/src/main.rs
@@ -1,3 +1,13 @@
+extern crate image;
+
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let image_path = env::args().skip(1).next().unwrap();
+    let path = Path::new(&image_path);
+
+    let img = image::open(path).unwrap();
+    let rotated = img.rotate90();
+    rotated.save(path).unwrap();
 }
