6c34fae2673d8e953caa0f7efc3536a8550e949c
Rahul Sharma <rsconceptx@gmail.com>
2018-07-28 23:17:57 +0530

    add bench_example

---
+++ Chapter03/bench_example/src/lib.rs
@@ -0,0 +1,25 @@
+// bench_example/src/lib.rs
+
+#![feature(test)]
+
+extern crate test;
+
+use test::Bencher;
+
+pub fn do_nothing_slowly() {
+    print!(".");
+    for _ in 1..10_000_000 {};
+}
+
+pub fn do_nothing_fast() { 
+}
+
+#[bench]
+fn bench_nothing_slowly(b: &mut Bencher) {
+    b.iter(|| do_nothing_slowly());
+}
+
+#[bench]
+fn bench_nothing_fast(b: &mut Bencher) {
+    b.iter(|| do_nothing_fast());
+}
