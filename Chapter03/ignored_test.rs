eb9df34418da8ea3619ff216b5d5d2406185a99e
Rahul Sharma <rsconceptx@gmail.com>
2018-07-31 01:49:09 +0530

    add ignored_test.rs

---
+++ Chapter03/ignored_test.rs
@@ -0,0 +1,17 @@
+// silly_loop.rs
+
+// compile in test mode: `rustc --test ignored_test.rs`
+// run tests using: `./ignored_test`
+
+pub fn silly_loop() {
+    for _ in 1..1_000_000_000 {};
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    #[ignore]
+    pub fn test_silly_loop() {
+        ::silly_loop();
+    }
+}
