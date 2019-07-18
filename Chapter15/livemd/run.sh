b5207b93ad8452d41dd25b0bb580639207f712ed
creativcoder <rahul.sharma@atherenergy.com>
2019-07-19 01:04:02 +0530

    Update Chapter15

--- Chapter15/livemd/run.sh
+++ Chapter15/livemd/run.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+# Prerequisites
+rustup target add wasm32-unknown-unknown || true
+# Update the dependency in Cargo.toml to match with the latest wasm-bindgen-cli tool
+cargo install -f wasm-bindgen-cli || true
 cargo build --target wasm32-unknown-unknown
 wasm-bindgen target/wasm32-unknown-unknown/debug/livemd.wasm --out-dir app
 cd app
